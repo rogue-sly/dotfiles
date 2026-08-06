396a785a3bcca4c752136871f6de202d54d212b3
rogue <rogue87@tuta.io>
2026-08-06 09:55:38 +0200

    add ripgrep config

--- profiles/laptop/user/fish/config.fish
+++ profiles/laptop/user/fish/config.fish
@@ -1,7 +1,9 @@
 if status is-interactive
     set -U fish_greeting
+
     set -gx EDITOR nvim
     set -gx VISUAL nvim
+    set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
 
     alias zj=zellij
 end
