e25320b14ed9d898099b884bb9f2e669069a0eb9
rogue <rogue87@tuta.io>
2026-08-07 08:09:45 +0200

    bleh

--- profiles/laptop/user/fish/config.fish
+++ profiles/laptop/user/fish/config.fish
@@ -1,9 +1,12 @@
 if status is-interactive
+    # disable annoying greeting
     set -U fish_greeting
-
+    # env vars
     set -gx EDITOR nvim
     set -gx VISUAL nvim
     set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
-
+    # aliases
     alias zj=zellij
+    # paths
+    fish_add_path ~/.local/bin
 end
