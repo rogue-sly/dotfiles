19a479cedb29f1bb0fff56e799a1bd0e9c2bddd5
rogue <rogue87@tuta.io>
2026-08-12 18:34:39 +0200

    update fish

--- profiles/laptop/user/fish/config.fish
+++ profiles/laptop/user/fish/config.fish
@@ -3,10 +3,11 @@ if status is-interactive
     set -U fish_greeting
     # env vars
     set -gx EDITOR nvim
-    set -gx VISUAL nvim
     set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
+    set -gx VISUAL nvim
     # aliases
     alias zj=zellij
+    alias mise-en="mise en --shell=fish"
     # paths
     fish_add_path ~/.local/bin
 end
