21e1f53fd5a25f9fa08a9f6b096c35f8212f5536
rogue <rogue87@tuta.io>
2026-08-19 15:39:34 +0200

    update dotbee config

--- profiles/global/nvim/lua/config/autocmds.lua
+++ profiles/global/nvim/lua/config/autocmds.lua
@@ -22,7 +22,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     pattern = "*",
     desc = "highlight selection on yank",
     callback = function()
-        vim.highlight.on_yank({ timeout = 200, visual = true })
+        vim.highlight.on_yank()
     end,
 })
 
