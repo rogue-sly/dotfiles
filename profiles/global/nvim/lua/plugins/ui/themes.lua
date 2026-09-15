7977db792635dee5e465ec10c70bc7e48402ee84
rogue <rogue87@tuta.io>
2026-09-15 12:10:46 +0200

    del themes

--- profiles/global/nvim/lua/plugins/ui/themes.lua
+++ profiles/global/nvim/lua/plugins/ui/themes.lua
@@ -1,6 +1,4 @@
 ---@type LazySpec
 return {
-    { "folke/tokyonight.nvim", version = false, cond = false },
-    { "rebelot/kanagawa.nvim", version = false, cond = false },
     { "EdenEast/nightfox.nvim", version = false, cond = true },
 }
